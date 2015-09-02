9975a25450235d1f2c52a09fd4ba23b5c7291c10
Bredoto <serg.rumyantsev@gmail.com>
2015-09-02 11:32:24 +0300

    Fibonacci2

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,6 +1,9 @@
 import math
-def Fibonacci(n):
+def FibonacciGold(n):
     fib = (1.0/math.sqrt(5))*math.pow( (1+ math.sqrt(5))/2,n) - (1.0/math.sqrt(5))*math.pow( (1 - math.sqrt(5))/2,n)
     print fib
-for i in range(1,20):
-    Fibonacci(i)
+def main():
+    for i in range(1,20):
+        FibonacciGold(i)
+
+main()
