77fa347e84987ed40f3e7a21b0fabff2efa6f6fa
Bredoto <serg.rumyantsev@gmail.com>
2015-09-02 11:27:46 +0300

    Fibonacci

---
+++ Fibonacci.py
@@ -0,0 +1,6 @@
+import math
+def Fibonacci(n):
+    fib = (1.0/math.sqrt(5))*math.pow( (1+ math.sqrt(5))/2,n) - (1.0/math.sqrt(5))*math.pow( (1 - math.sqrt(5))/2,n)
+    print fib
+for i in range(1,20):
+    Fibonacci(i)
